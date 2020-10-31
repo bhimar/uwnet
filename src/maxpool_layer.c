7d00b0a993f5c9cf9b6b80693201b0717a882a66
bhimar <Rehaan@DESKTOP-7HDI3EJ.localdomain>
2020-10-31 15:25:10 -0700

    finished part 6 and passed tests

--- src/maxpool_layer.c
+++ src/maxpool_layer.c
@@ -33,7 +33,7 @@ matrix forward_maxpool_layer(layer l, matrix in)
             for(int i = 0; i < l.height; i+= l.stride) {
                 for(int j = 0; j < l.width; j+= l.stride) {
                     
-                    float max_el = in.data[img * img_size + k * l.width * l.height + i * l.width + j];
+                    float max_el = FLT_MIN;
                     for (int m = i - (l.size / 2) + (1 - (l.size % 2)); m <= i + (l.size / 2); m++) {
                         for (int n = j - (l.size / 2) + (1 - (l.size % 2)); n <= j + (l.size / 2); n++) {
                             if (m >= 0 && n >= 0 && m < l.height && n < l.width) {
@@ -61,15 +61,49 @@ matrix forward_maxpool_layer(layer l, matrix in)
 // matrix dy: error term for the previous layer
 matrix backward_maxpool_layer(layer l, matrix dy)
 {
-    matrix in    = *l.x;
+    matrix in = *l.x;
     matrix dx = make_matrix(dy.rows, l.width*l.height*l.channels);
 
+    int img_size = dx.cols;
     int outw = (l.width-1)/l.stride + 1;
     int outh = (l.height-1)/l.stride + 1;
+    int out_cols =  outw*outh*l.channels;
     // TODO: 6.2 - find the max values in the input again and fill in the
     // corresponding delta with the delta from the output. This should be
     // similar to the forward method in structure.
 
+     for(int img = 0; img < in.rows; img++) {
+        // for every channel in an image
+        int out_idx = 0;
+        for (int k = 0; k < l.channels; k++) {
+            // track center of pooling filter
+            for(int i = 0; i < l.height; i+= l.stride) {
+                for(int j = 0; j < l.width; j+= l.stride) {
+                    
+                    float max_el = FLT_MIN;
+                    int max_ind = img * img_size + k * l.width * l.height + i * l.width + j; 
+
+                    for (int m = i - (l.size / 2) + (1 - (l.size % 2)); m <= i + (l.size / 2); m++) {
+                        for (int n = j - (l.size / 2) + (1 - (l.size % 2)); n <= j + (l.size / 2); n++) {
+
+                            if (m >= 0 && n >= 0 && m < l.height && n < l.width) {
+                              float el = in.data[img * img_size + k * l.width * l.height + m * l.width + n];
+                              int ind = img * img_size + k * l.width * l.height + m * l.width + n;
+                              if (el > max_el) {
+                                  max_el = el;
+                                  max_ind = ind;
+                              }
+                            }
+
+                        }
+                    }
+                    // populate out
+                    dx.data[max_ind] += dy.data[img * out_cols + out_idx];
+                    out_idx++;
+                }
+            }
+        }
+    }
 
 
     return dx;
